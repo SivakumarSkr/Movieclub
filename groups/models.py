74e80b69f1892c778dc871c472aaa5b02f390b5a
sivakumar <sivakumar28skr@gmail.com>
2020-02-04 01:48:33 +0530

    permissions for groups.
    Signed-off-by: sivakumar <sivakumar28skr@gmail.com>

--- groups/models.py
+++ groups/models.py
@@ -1,10 +1,7 @@
-
 import uuid
 
 from django.conf import settings
 from django.db import models
-from django.db.models import signals
-from django.dispatch import receiver
 from django.utils.timezone import now
 
 from contents.models import Content
@@ -113,6 +110,9 @@ class ClosedGroup(Group):
         admins = self.get_admins()
         if admins.filter(pk=user.pk).exists() or user == self.creator:
             return True
+
+    class Meta:
+        proxy = True
 
 
 class GroupBlog(Content):
