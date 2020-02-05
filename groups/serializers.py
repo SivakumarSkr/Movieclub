6a4c902886244f9277b3fe6374edd8175d6b5ce6
Sivakumar R <48449224+SivakumarSkr@users.noreply.github.com>
2020-02-06 03:14:43 +0530

    Update serializers.py

--- groups/serializers.py
+++ groups/serializers.py
@@ -1,17 +1,45 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
+from taggit_serializer.serializers import TaggitSerializer
 
-from groups.models import Group, GroupBlog
+from groups.models import Group, GroupBlog, JoinRequest, ClosedGroup
 
 
 class GroupSerializer(ModelSerializer):
     """ Serializer for Group"""
+
     class Meta:
         model = Group
-        fields = ('name', 'time_created', 'creator', 'admins', 'type_of_group', 'description', )
+        fields = ('uuid_id', 'name', 'time_created', 'type_of_group', 'creator', 'description')
+        read_only_fields = ('uuid_id', 'time_created', 'creator', 'type_of_group')
 
 
-class GroupBlogSerializer(ModelSerializer):
+class ClosedGroupSerializer(ModelSerializer):
+    class Meta:
+        model = ClosedGroup
+        fields = ('uuid_id', 'name', 'time_created', 'type_of_group', 'creator', 'admins', 'description')
+        read_only_fields = ('uuid_id', 'time_created', 'creator', 'type_of_group', 'admins')
+
+
+class TagSerializerField(serializers.ListField):
+    child = serializers.CharField()
+
+    def to_representation(self, data):
+        return data.values_list('name', flat=True)
+
+
+class GroupBlogSerializer(TaggitSerializer, ModelSerializer):
     """ Serializer for Group Blog"""
+
+    # tags = TagListSerializerField()
+
     class Meta:
         model = GroupBlog
-        fields = ('tags', 'contents', 'image', 'heading')
+        fields = ('contents', 'image', 'heading', 'group')
+
+
+class JoinRequestSerializer(ModelSerializer):
+    class Meta:
+        model = JoinRequest
+        fields = '__all__'
+        read_only_fields = '__all__'
