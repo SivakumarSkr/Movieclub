28501f7dbe600b55de49ef7371bbe276977603d5
Sivakumar R <48449224+SivakumarSkr@users.noreply.github.com>
2020-02-06 03:24:02 +0530

    Update serializers.py

--- groups/serializers.py
+++ groups/serializers.py
@@ -2,15 +2,23 @@ from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from taggit_serializer.serializers import TaggitSerializer
 
-from groups.models import Group, GroupBlog, JoinRequest
+from groups.models import Group, GroupBlog, JoinRequest, ClosedGroup
 
 
 class GroupSerializer(ModelSerializer):
     """ Serializer for Group"""
+
     class Meta:
         model = Group
-        fields = ('uuid_id', 'name', 'time_created', 'creator', 'admins', 'type_of_group', 'description')
-        read_only_fields = ('uuid_id', 'time_created', 'creator', 'admins')
+        fields = ('uuid_id', 'name', 'time_created', 'type_of_group', 'creator', 'description')
+        read_only_fields = ('uuid_id', 'time_created', 'creator', 'type_of_group')
+
+
+class ClosedGroupSerializer(ModelSerializer):
+    class Meta:
+        model = ClosedGroup
+        fields = ('uuid_id', 'name', 'time_created', 'type_of_group', 'creator', 'admins', 'description')
+        read_only_fields = ('uuid_id', 'time_created', 'creator', 'type_of_group', 'admins')
 
 
 class TagSerializerField(serializers.ListField):
