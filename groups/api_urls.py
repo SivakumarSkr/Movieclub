8ff8cae9f078138fe87f7ede3237064b450d5a12
Sivakumar R <48449224+SivakumarSkr@users.noreply.github.com>
2020-02-06 03:21:43 +0530

    Update api_urls.py

--- groups/api_urls.py
+++ groups/api_urls.py
@@ -1,10 +1,11 @@
-from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from groups import api_view
 
 router = DefaultRouter()
 router.register('groups', api_view.GroupVewSet, base_name='group')
+router.register('closed-groups', api_view.ClosedGroupViewSet, base_name='closed-group')
+router.register('join-requests', api_view.JoinRequestViewSet, base_name='requests')
 router.register('group_blog', api_view.GroupBlogViewSet, base_name='group_blog')
 urlpatterns = [
 ]
